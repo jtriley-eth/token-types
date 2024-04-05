a97a6bf84ac6676c7b8d463133bfa5f36c060ffa
jtriley.eth <Jtriley15@gmail.com>
2024-04-05 09:08:40 -0700

    nit

--- src/ERC6909.sol
+++ src/ERC6909.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-
 // ## ERC6909 Type Wrapper
 //
 // This type wraps the address primitive type and contains methods to call the core ERC6909
